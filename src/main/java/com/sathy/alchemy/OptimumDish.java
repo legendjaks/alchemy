a51519498da7a849d276cc2cd384693f7fc30a59
legendjaks <cjayaprakash@gmail.com>
2015-08-14 11:21:21 +0530

    Added comments

--- src/main/java/com/sathy/alchemy/OptimumDish.java
+++ src/main/java/com/sathy/alchemy/OptimumDish.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.Stack;
 
+// http://www.careercup.com/question?id=5116908397395968
 public class OptimumDish {
   
   private Set<Integer> result = new HashSet<Integer>();
@@ -13,16 +14,19 @@ public class OptimumDish {
       System.out.print(r + " ");
   }
 
+  // Find the optimum dish by navigating all available options
   public void find(int[][] m, int r, int c, int mr, int mc, Stack<Integer> dishes) {
 
     dishes.push(c);
     
     if (r == mr) {
+      // Reached last person. Get the unique dishes
       Set<Integer> d = new HashSet<>(dishes);
       if(result.size() == 0 || result.size() > d.size())
         result = d;
     }
     else if (r < mr) {
+      // Check next person's preferred dish
       for (int i = 0; i <= mc; i++) {
         if (m[r + 1][i] == 1) {
           find(m, r+1, i, mr, mc, dishes);
@@ -33,6 +37,8 @@ public class OptimumDish {
 
     dishes.pop();
     
+    // Current dish may not be the optimum.
+    // Check other dish for the same person
     for (int i = c + 1; i <= mc; i++) {
       if (m[r][i] == 1) {
         find(m, r, i, mr, mc, dishes);
